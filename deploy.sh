1fb79d800af98ec04225a4319895bea08758b349
Andres Celis <andres.celis@mayasoft.com.co>
2025-11-26 16:21:09 -0500

    feat: update deploy script to use 'docker compose' command syntax

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-docker-compose build
-docker-compose up -d
-docker-compose exec -T app npx prisma migrate deploy --schema=./prisma/schema.prisma
+docker compose build
+docker compose up -d
+docker compose exec -T app npx prisma migrate deploy --schema=./prisma/schema.prisma
