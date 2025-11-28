40ac0aecce124564796e79eface611f42c5057b5
Andrés Celis <andres.celis@mayasoft.com.co>
2025-11-28 14:53:28 -0500

    refactor: reorder fields in Coke and Postobon enums for consistency and clarity

--- src/modules/validator/documents/coke/coke.fields.ts
+++ src/modules/validator/documents/coke/coke.fields.ts
@@ -1,8 +1,8 @@
 export enum CokeHeaderFields {
   FECHA_FACTURA = 'fecha_factura',
   NUMERO_FACTURA = 'numero_factura',
-  VALOR_TOTAL_FACTURA = 'valor_total_factura',
   RAZON_SOCIAL = 'razon_social',
+  VALOR_TOTAL_FACTURA = 'valor_total_factura',
 }
 
 export enum CokeBodyFields {
@@ -10,9 +10,10 @@ export enum CokeBodyFields {
   ITEM_DESCRIPCION_PRODUCTO = 'item_descripcion_producto',
   TIPO_EMBALAJE = 'tipo_embalaje',
   UNIDADES_VENDIDAS = 'unidades_vendidas',
-  VALOR_UNITARIO_ITEM = 'valor_unitario_item',
   VALOR_VENTA_ITEM = 'valor_venta_item',
   VALOR_IBUA_Y_OTROS = 'valor_ibua_y_otros',
   UNIDADES_EMBALAJE = 'unidades_embalaje',
+
+  VALOR_UNITARIO_ITEM = 'valor_unitario_item',
 }
 
