619bb1b22e1df9d810a843cea3a0a960a80e2f6b
Andrés Celis <andres.celis@mayasoft.com.co>
2025-12-05 08:48:10 -0500

    feat: femsa new calculations

--- src/modules/validator/documents/femsa/femsa.fields.ts
+++ src/modules/validator/documents/femsa/femsa.fields.ts
@@ -4,6 +4,8 @@ export enum FemsaHeaderFields {
   VALOR_TOTAL_FACTURA = 'valor_total_factura',
   RAZON_SOCIAL = 'razon_social',
   TOTAL_FACTURA_SIN_IVA = 'total_factura_sin_iva',
+  IVA_TARIFA_GENERAL = 'iva_tarifa_general',
+  IBUA = 'ibua',
 }
 
 export enum FemsaBodyFields {
@@ -15,6 +17,8 @@ export enum FemsaBodyFields {
   VALOR_VENTA_ITEM = 'valor_venta_item',
   VALOR_IBUA_Y_OTROS = 'valor_ibua_y_otros',
   UNIDADES_EMBALAJE = 'unidades_embalaje',
+  CANTIDAD = 'cantidad',
+  VALOR_DESCUENTO = 'valor_descuento',
 }
 
 export const FEMSA_THRESOLDS = {
@@ -25,10 +29,10 @@ export const FEMSA_THRESOLDS = {
   [FemsaHeaderFields.TOTAL_FACTURA_SIN_IVA]: 0.88,
   [FemsaBodyFields.CODIGO_PRODUCTO]: 0.95,
   [FemsaBodyFields.ITEM_DESCRIPCION_PRODUCTO]: 0.92,
-  [FemsaBodyFields.TIPO_EMBALAJE]: 0.88,
   [FemsaBodyFields.UNIDADES_VENDIDAS]: 0.83,
+  [FemsaBodyFields.UNIDADES_EMBALAJE]: 0.85,
+  [FemsaBodyFields.TIPO_EMBALAJE]: 0.88,
   [FemsaBodyFields.VALOR_UNITARIO_ITEM]: 0.93,
   [FemsaBodyFields.VALOR_VENTA_ITEM]: 0.9,
   [FemsaBodyFields.VALOR_IBUA_Y_OTROS]: 0.89,
-  [FemsaBodyFields.UNIDADES_EMBALAJE]: 0.85,
 };
